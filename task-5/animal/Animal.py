cd566679853e8f294ffd15e45ccdee3937257b29
Voronin Alexandr <info@z-site.ru>
2018-09-12 04:41:52 +0300

    Init

---
+++ task-5/animal/Animal.py
@@ -0,0 +1,130 @@
+class Animal():
+    __weight = 0
+    __name = None
+
+    def __init__(self, params):
+        self.__weight = float(params['weight'])
+        self.__name = params['name']
+
+    @property
+    def weight(self):
+        return self.__weight
+
+    @property
+    def name(self):
+        return self.__name
+
+    def feed(self):
+        print('Покормили')
+                
+
+class Animal_weight():
+    """Класс для работы с весом животных"""
+    animals = list()
+    __heaviest_animal = None
+    __full_weight = float(0)
+
+    def __init__(self, animals: tuple):
+        for animal in animals:
+            self.__add(animal)
+
+
+    def __set_heaviest_animal(self, animal: Animal):
+        if not self.__heaviest_animal:
+            self.__heaviest_animal = animal
+        if self.__heaviest_animal.weight < animal.weight:
+            self.__heaviest_animal = animal
+    
+    def __add(self, animal: Animal):
+        self.__set_heaviest_animal(animal)
+        self.__full_weight += float(animal.weight)
+        self.animals.append(animal)
+
+    @property
+    def heaviest_animal(self):
+        return self.__heaviest_animal
+
+    @property
+    def full_weight(self):
+        return self.__full_weight
+
+class Egg():
+    """ Получить яйца с животного """
+    __allowed = True
+    
+    def get_egg(self):
+        
+        if self.__allowed:
+            print('Собрали яйца')
+        else:
+            print('Нет яиц')
+        self.__allowed = False
+
+class Vote():
+    """ Голос животного """
+    def vote(self):
+        pass
+
+class Milk():
+    """ Доить молоко """  
+    __allowed = True
+    
+    def get_milk(self):
+        if self.__allowed:
+            print('Подоили молоко')
+        else:
+            print('Нет молока')
+        self.__allowed = False
+        
+class Shearing():
+    """ Стричь животное """
+    __allowed = True
+    def cut(self):
+        if self.__allowed:
+            print('Подстригли')
+        else:
+            print('Нет шерсти')
+        self.__allowed = False        
+        
+class Bird(Animal, Egg):
+    """ Птицы """
+    pass    
+
+
+class Goose(Bird, Vote):
+   """ Гусь - несет яйца """
+   def vote(self):
+       return 'Га-га-га'
+
+class Hen(Bird, Vote):
+    """ Курица - несет яйца """
+    def vote(self):
+        return 'Ко-ко-ко'
+                      
+class Cow(Animal, Vote, Milk):
+    """ Корова - дает молоко """
+    def vote(self):
+        return 'Мууууу'
+
+                      
+
+class Sheep(Animal, Vote, Shearing):
+    """ Бараны - дают шерсть """
+    def vote(self):
+        return 'Беееее' 
+
+class Goat(Animal, Vote, Milk):
+    """ Козы - дают молоко """
+    def vote(self):
+        return 'Меееее'
+        
+
+class Duck(Bird, Vote):
+    """ Утка - несет яйца """ 
+    def vote(self):
+        return 'Кря-Кря'    
+
+class Turtle(Animal, Egg):
+    """Черепаха - несет яйца"""
+    pass
+        
---
+++ task-5/main.py
@@ -0,0 +1,114 @@
+from animal.Animal import *
+
+
+animals_on_farm = [
+  {'type':'Goose'
+   , 'init':{'name':'Серый гусь', 'weight':12}
+  }
+  
+  , {'type':'Goose'
+   , 'init':{'name':'Белый гусь', 'weight':10}
+  } 
+  
+  , {'type':'Hen'
+   , 'init':{'name':'Ко-Ко', 'weight':1}
+  } 
+
+  , {'type':'Hen'
+   , 'init':{'name':'Кукареку', 'weight':12}
+  }
+
+  , {'type':'Cow'
+   , 'init':{'name':'Манька', 'weight':150}
+  }  
+
+  , {'type':'Sheep'
+   , 'init':{'name':'Барашек', 'weight':40}
+  } 
+
+  , {'type':'Sheep'
+   , 'init':{'name':'Кудрявый', 'weight':50}
+  } 
+
+  , {'type':'Goat'
+   , 'init':{'name':'Рога', 'weight':50}
+  }    
+   
+  , {'type':'Goat'
+   , 'init':{'name':'Копыта', 'weight':45}
+  }  
+
+  , {'type':'Duck'
+   , 'init':{'name':'Кряква', 'weight':5}
+  } 
+
+  , {'type':'Turtle'
+   , 'init':{'name':'Леонардо', 'weight':0.9}
+  }           
+]
+
+
+animals = list()
+for animal_on_farm in animals_on_farm:
+    _class = globals()[animal_on_farm['type']]
+    animal = _class(animal_on_farm['init'])
+    animals.append(animal)
+
+
+animal_weight = Animal_weight(animals)
+
+
+for animal in animals:
+    print(type(animal), animal.name)
+    animal.feed()
+    if isinstance(animal, Egg):
+        animal.get_egg()
+    if isinstance(animal, Vote):
+        print(animal.vote())
+    if isinstance(animal, Milk):
+        animal.get_milk() 
+    if isinstance(animal, Shearing):
+        animal.cut()               
+
+    print()
+
+
+print('Самый тяжелый:', animal_weight.heaviest_animal.name, '', animal_weight.heaviest_animal.weight)
+print('Общий вес:', animal_weight.full_weight)
+
+
+
+
+# Домашнее задание к лекции 1.5 «Классы и их применение в Python»
+
+# Вы приехали помогать на ферму Дядюшки Джо и видите вокруг себя множество разных животных:
+# * гусей "Серый" и "Белый"
+# * корову "Маньку"
+# * овец "Барашек" и "Кудрявый"
+# * кур "Ко-Ко" и "Кукареку"
+# * коз "Рога" и "Копыта"
+# * и утку "Кряква"
+
+# Со всеми животными вам необходимо как-то взаимодействовать:
+# * кормить
+# * корову и коз доить
+# * овец стричь
+# * собирать яйца у кур, утки и гусей
+# * различать по голосам(коровы мычат, утки крякают и т.д.)
+
+# ## Задача №1
+# Нужно реализовать классы животных, не забывая использовать наследование, определить *общие методы* взаимодействия с животными и 
+# дополнить их в дочерних классах, если потребуется.
+
+# ## Задача №2
+# Для каждого животного из списка должен существовать экземпляр класса.
+# Каждое животное требуется накормить и подоить/постричь/собрать яйца, если надо.
+
+# ## Задача №3
+# У каждого животного должно быть определено имя(self.name) и вес(self.weight). 
+# - Необходимо посчитать общий вес всех животных(экземпляров класса);
+# - Вывести название самого тяжелого животного.
+
+# ## Задача №4
+# Для подготовки к следующей лекции прочитайте про [работу с файлами](https://pythonworld.ru/tipy-dannyx-v-python/fajly-rabota-s-fajlami.html).
+
