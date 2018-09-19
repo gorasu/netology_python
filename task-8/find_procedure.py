4ebf3af4f06a9236a29818c1d203770412879f42
Voronin Alexandr <info@z-site.ru>
2018-09-19 17:50:34 +0300

    получение файлов директории

--- task-8/find_procedure.py
+++ task-8/find_procedure.py
@@ -1,16 +1,34 @@
+from pprint import pprint
 from typing import List
+import os
 
 
 class File:
 
+    def __init__(self, path):
+        self.__path = path
+
     def get_path(self) -> str:
-        pass
+        return self.__path
 
 
 class Files:
 
+    def __init__(self, dir_path):
+        if not os.path.isdir(dir_path):
+            raise NotADirectoryError()
+        self.__dir_path = dir_path
+
+    def __join_path(self, path):
+        return os.path.join(self.__dir_path, path)
+
     def get_files(self) -> List[File]:
-        pass
+        result = list()
+        for entity in os.listdir(self.__dir_path):
+            entity_path = self.__join_path(entity)
+            if os.path.isfile(entity_path):
+                result.append(File(entity_path))
+        return result
 
 
 class SearchResult:
@@ -68,6 +86,13 @@ class Command:
             self.start()
         print('Нашли в')
         print(result.get_files())
+
+
+file_ser = Files('./Migrations/')
+pprint(file_ser.get_files())
+
+
+#command = Command()
 
 # word = input('Введите строку поиска:')
 # command = Command(FileForSearch())
