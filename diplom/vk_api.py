3ba46ff67af8117109cd23a04b5509a62fc67f99
Voronin Alexandr <info@z-site.ru>
2018-10-04 05:54:52 +0300

    Что бы приложение поднимало вызов обратно когда достигнут временной лимит запросов

--- diplom/vk_api.py
+++ diplom/vk_api.py
@@ -1,3 +1,4 @@
+import time
 import urllib.parse
 import requests
 
@@ -12,8 +13,23 @@ class VkApiError(Exception):
         return self.__code
 
 
-class VkApi:
+class VkApiTimeLimitError(VkApiError):
+    pass
 
+
+class VkApiErrorFactory:
+
+    def __init__(self, error: dict):
+        self.__error = error
+
+    def get_exception(self):
+        if self.__error['error_code'] == 6:
+            return VkApiTimeLimitError(self.__error['error_msg'], self.__error['error_code'])
+
+        return VkApiError(self.__error['error_msg'], self.__error['error_code'])
+
+
+class VkApi:
     __api_url = 'https://api.vk.com/method/'
 
     def __init__(self, token, version='5.85'):
@@ -23,11 +39,19 @@ class VkApi:
     def __main_params(self):
         return {'access_token': self.__token, 'v': self.__version}
 
-    def get(self, method, params: dict = {}):
+    def _call(self, method, params):
         params.update(self.__main_params())
         request_url = urllib.parse.urljoin(self.__api_url, method)
         response = requests.get(request_url, params)
         result = response.json()
         if result.get('error'):
-            raise VkApiError(result['error']['error_msg'], result['error']['error_code'])
+            raise VkApiErrorFactory(result['error']).get_exception()
+
         return result['response']
+
+    def get(self, method, params: dict = {}):
+        try:
+            return self._call(method, params)
+        except VkApiTimeLimitError as e:
+            time.sleep(1)
+            return self.get(method, params)
